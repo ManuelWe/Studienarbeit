59f58ab34ebc1618cede2254bfdcd7be5f5ff5ef
Wetzel <manuel.wetzel@hpe.com>
2020-01-31 19:41:42 +0100

    Dashboard and products page; api call

--- src/router/index.js
+++ src/router/index.js
@@ -1,26 +1,25 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
-import Home from '../views/Home.vue';
+import Dashboard from '../views/Dashboard.vue';
+import Products from '../views/Products.vue';
 
 Vue.use(VueRouter);
 
 const routes = [
   {
     path: '/',
-    name: 'home',
-    component: Home,
+    name: 'dashboard',
+    component: Dashboard,
   },
   {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue'),
+    path: '/produkte',
+    name: 'products',
+    component: Products,
   },
 ];
 
 const router = new VueRouter({
+  mode: 'history',
   routes,
 });
 
---
+++ src/views/Dashboard.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="dashboard">
+    <div class="album py-5 bg-light">
+      <div class="container">
+        <div class="row">
+          <div
+            v-for="(category, index) in categories"
+            :key="index"
+            class="col-md-4"
+          >
+            <DashboardCard :category="category" />
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import DashboardCard from '@/components/DashboardCard.vue';
+
+export default {
+  components: {
+    DashboardCard,
+  },
+  data() {
+    return {
+      categories: ['Produkte', 'Rest', 'Bla', 'lala', '123'],
+    };
+  },
+};
+</script>
