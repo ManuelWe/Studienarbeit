5ca16e122b615b868861cb51f2e9189a04af9edb
Wetzel <manuel.wetzel@hpe.com>
2020-05-12 17:03:45 +0200

    Added search field for kreuztabelle

--- Application/src/views/Products.vue
+++ Application/src/views/Products.vue
@@ -6,7 +6,7 @@
           v-model="searchString"
           class="input"
           aria-label="Suchfeld"
-          placeholder="Search..."
+          placeholder="Suche..."
           type="search"
           icon-pack="fas"
           icon="search"
