5f20efe000e20bff3106db4c36ebff1aca6e09e5
Wetzel <manuel.wetzel@hpe.com>
2020-05-13 22:09:12 +0200

    moved navigationButtons to the sides

--- Application/src/views/Product.vue
+++ Application/src/views/Product.vue
@@ -1,57 +1,64 @@
 <template>
-  <div id="product">
-    <h1 class="is-size-1">
-      {{ product.Artikelbezeichnung }}
-    </h1>
-    <button
-      :disabled="getFilteredProducts[0].fields.ArtNr === ArtNr"
-      class="button returnButton"
-      @click="lastProduct()"
-    >
-      <span class="icon is-small">
-        <i class="fas fa-arrow-left fa-2x" />
-      </span>
-    </button>
-    <button
-      :disabled="getFilteredProducts[getFilteredProducts.length - 1].fields.ArtNr === ArtNr"
-      class="button returnButton"
-      @click="nextProduct()"
-    >
-      <span class="icon is-small">
-        <i class="fas fa-arrow-right fa-2x" />
-      </span>
-    </button>
-    <h2
-      v-if="product.Produktinfo"
-      class="is-size-2"
-    >
-      {{ product.Produktinfo }}
-    </h2>
-    <h3 class="is-size-3">
-      Gewicht: {{ product.Artikelgewicht }}g
-    </h3>
-    <h5 class="is-size-5">
-      Zutaten:
-    </h5>
-    <p>{{ product.Zutaten }}</p>
-    <div class="columns">
-      <div class="column">
-        <figure class="image is-512x512">
-          <img
-            :src="getImage(ArtNr)"
-          >
-        </figure>
-      </div>
-      <div class="column">
-        <NutritionalTable :product="product" />
-      </div>
+  <div
+    v-if="product"
+    id="product"
+    class="columns"
+  >
+    <div class="column">
+      <button
+        :disabled="getFilteredProducts[0].fields.ArtNr === ArtNr"
+        class="button navigationButton"
+        @click="lastProduct()"
+      >
+        <span class="icon is-small">
+          <i class="fas fa-arrow-left fa-2x" />
+        </span>
+      </button>
     </div>
-    <h4 class="is-size-4">
-      Allergene: {{ product.AllergeneAlle }}
-    </h4>
-    <h4 class="is-size-4">
-      Zusatzstoffe: {{ product.ZusatzstoffeAlle }}
-    </h4>
+    <div class="column is-10">
+      <h2
+        v-if="product.Produktinfo"
+        class="is-size-2"
+      >
+        {{ product.Produktinfo }}
+      </h2>
+      <h3 class="is-size-3">
+        Gewicht: {{ product.Artikelgewicht }}g
+      </h3>
+      <h5 class="is-size-5">
+        Zutaten:
+      </h5>
+      <p>{{ product.Zutaten }}</p>
+      <div class="columns">
+        <div class="column">
+          <figure class="image is-512x512">
+            <img
+              :src="getImage(ArtNr)"
+            >
+          </figure>
+        </div>
+        <div class="column">
+          <NutritionalTable :product="product" />
+        </div>
+      </div>
+      <h4 class="is-size-4">
+        Allergene: {{ product.AllergeneAlle }}
+      </h4>
+      <h4 class="is-size-4">
+        Zusatzstoffe: {{ product.ZusatzstoffeAlle }}
+      </h4>
+    </div>
+    <div class="column">
+      <button
+        :disabled="getFilteredProducts[getFilteredProducts.length - 1].fields.ArtNr === ArtNr"
+        class="button navigationButton"
+        @click="nextProduct()"
+      >
+        <span class="icon is-small">
+          <i class="fas fa-arrow-right fa-2x" />
+        </span>
+      </button>
+    </div>
   </div>
 </template>
 
@@ -81,7 +88,12 @@ export default {
     },
   },
   created() {
-    this.product = this.getProduct(this.ArtNr);
+    const queriedProducts = this.getProduct(this.ArtNr);
+    if (queriedProducts) {
+      this.product = queriedProducts;
+    } else {
+      this.$router.replace({ name: 'products' });
+    }
   },
   beforeRouteUpdate(to, from, next) {
     this.ArtNr = to.params.ArtNr;
@@ -110,3 +122,11 @@ export default {
   },
 };
 </script>
+
+<style scoped>
+.navigationButton {
+  position: fixed;
+  top: 50%;
+  height: 7rem;
+}
+</style>
